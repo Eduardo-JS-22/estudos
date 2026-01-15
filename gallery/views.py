82f21f756a627f5f7277d37922f5248b80b688d0
Eduardo <eduardoj.schroeder@gmail.com>
2026-01-15 16:06:49 -0300

    Add new images, styles, and gallery templates; remove old index.html
    - Added multiple image files to the gallery and popular sections.
    - Introduced a new CSS file for styling the application.
    - Created new HTML templates for the gallery and individual image display.
    - Removed the outdated index.html file.

--- gallery/views.py
+++ gallery/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'gallery/index.html')
+
+def image(request):
+    return render(request, 'gallery/image.html')
